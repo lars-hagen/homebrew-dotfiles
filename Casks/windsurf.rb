7aef095d4e992454dbb658f1a557f154e0e1c4de
Lars Hagen <larsh2690@hotmail.com>
2024-11-27 12:45:20 +0100

    refactor: Update Windsurf cask with version 1.0.4 and correct download URL

--- Casks/windsurf.rb
+++ Casks/windsurf.rb
@@ -1,8 +1,9 @@
 cask "windsurf" do
-  version :latest
-  sha256 :no_check
+  version "1.0.4"
+  sha256 "a9eba9ef8938cc498a30ffcf95265a5c07b20362081f79d5467a5f238df30b63"
 
-  url "https://windsurf-stable.codeium.com/api/update/darwin-arm64-dmg/stable/latest"
+  url "https://windsurf-stable.codeiumdata.com/darwin-arm64-dmg/stable/ee432f33493a26d92a179ba5df483bdc6b8aa047/Windsurf-darwin-arm64-1.0.4.dmg",
+      verified: "windsurf-stable.codeiumdata.com/"
   name "Windsurf"
   desc "Windsurf Editor by Codeium"
   homepage "https://codeium.com/windsurf"
@@ -10,4 +11,4 @@ cask "windsurf" do
   auto_updates true
 
   app "Windsurf.app"
-end 
+end
