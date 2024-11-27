408bca06231cd09e9a797dc1f36bcb1a55941c41
Lars Hagen <larsh2690@hotmail.com>
2024-11-27 12:32:43 +0100

    fix: update windsurf cask with proper content

--- Casks/windsurf.rb
+++ Casks/windsurf.rb
@@ -1,1 +1,13 @@
- 
+cask "windsurf" do
+  version :latest
+  sha256 :no_check
+
+  url "https://windsurf-stable.codeium.com/api/update/darwin-arm64-dmg/stable/latest"
+  name "Windsurf"
+  desc "Windsurf Editor by Codeium"
+  homepage "https://codeium.com/windsurf"
+
+  auto_updates true
+
+  app "Windsurf.app"
+end 
